494486e431179b4adbcc6356ab472112a09140b9
rbox <androidrbox@gmail.com>
2014-07-19 22:45:51 -0700

    Let KeyBindings handle long presses.
    Move the RecentsOnMenuLong code into KeyBindings, make it the default
    for long press on Home, but allow key_bindings.xml to remap it and
    any other long press.

--- src/rbox/xposed/firetvmods/KeyBindings.java
+++ src/rbox/xposed/firetvmods/KeyBindings.java
@@ -1,10 +1,10 @@
 package rbox.xposed.firetvmods;
 
 import java.io.File;
-import java.util.Hashtable;
 import java.util.Map.Entry;
 
 import android.content.Context;
+import android.util.SparseArray;
 import android.view.KeyEvent;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -16,16 +16,29 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class KeyBindings implements IXposedHookZygoteInit, IXposedHookLoadPackage
 {
-	Hashtable<Integer, String> bindings;
+	private static final int LONG_PRESS = 0x80000000;
+	SparseArray<String> bindings;
 
 	@Override
 	public void initZygote(StartupParam startupParam) throws Throwable
 	{
-		bindings = new Hashtable<Integer, String>();
+		bindings = new SparseArray<String>();
+
+		// Add the home long press to recents by default
+		bindings.put(LONG_PRESS | KeyEvent.KEYCODE_HOME, null);
 
 		XSharedPreferences prefs = new XSharedPreferences(new File("/data/media/0/key_bindings.xml"));
 		for (Entry<String, ?> e : prefs.getAll().entrySet())
-			bindings.put(XposedHelpers.getStaticIntField(KeyEvent.class, e.getKey()), (String)e.getValue());
+		{
+			String key = e.getKey();
+			int longPress = 0;
+			if (key.endsWith("_LONG"))
+			{
+				longPress = LONG_PRESS;
+				key = key.substring(0, key.indexOf("_LONG"));
+			}
+			bindings.put(longPress | XposedHelpers.getStaticIntField(KeyEvent.class, key), (String)e.getValue());
+		}
 	}
 
 	@Override
@@ -35,6 +48,19 @@ public class KeyBindings implements IXposedHookZygoteInit, IXposedHookLoadPackag
 		if (!lpparam.packageName.equals("android") || bindings.size() == 0)
 			return;
 
+		// Add a hook for setting up recents on menu long press
+		if (bindings.get(LONG_PRESS | KeyEvent.KEYCODE_HOME) == null)
+		{
+			XposedHelpers.findAndHookConstructor("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader, new XC_MethodHook() {
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable
+				{
+					// Set the long press behavior to bring up the Recents dialog
+					XposedHelpers.setIntField(param.thisObject, "mLongPressOnHomeBehavior", 1);
+				}
+			});
+		}
+
 		// For some reason, findAndHookMethod doesn't work for this
 		XposedBridge.hookAllMethods(XposedHelpers.findClass("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader), "interceptKeyBeforeDispatching", new XC_MethodHook() {
 			@Override
@@ -43,7 +69,8 @@ public class KeyBindings implements IXposedHookZygoteInit, IXposedHookLoadPackag
 				KeyEvent event = (KeyEvent)param.args[1];
 				if (event.getAction() == KeyEvent.ACTION_DOWN)
 				{
-					String value = bindings.get(event.getKeyCode());
+					int longPress = (event.getFlags() & KeyEvent.FLAG_LONG_PRESS) != 0 ? LONG_PRESS : 0;
+					String value = bindings.get(longPress | event.getKeyCode());
 					if (value != null)
 					{
 						Context mContext = (Context)XposedHelpers.getObjectField(param.thisObject, "mContext");
